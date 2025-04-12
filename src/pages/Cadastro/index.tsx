a3319f6e8cc42709a7c320821c8cf35eeff991cb
Juan Veronez <juan.monteirov@gmail.com>
2025-04-12 14:48:28 -0300

    feat: add `react-input-mask` and create a new component to use this masked input

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -1,10 +1,10 @@
 import { Logotipo } from "../../components";
-import CadastroEndereco from "./CadastroEndereco";
+import CadastroPessoal from "./CadastroPessoal";
 export default function Cadastro() {
   return (
     <>
       <Logotipo />
-      <CadastroEndereco />
+      <CadastroPessoal />
     </>
   );
 }
