ad381cca5f748d86b9a4912e212572876a22d9b0
Juan Veronez <juan.monteirov@gmail.com>
2025-04-14 10:52:00 -0300

    feat(rhf-with-zod): use `refine` to validate schema object based on `senha` and `senhaVerificada`

--- forms/rhf-with-zod/src/pages/Cadastro/CadastroPessoal.tsx
+++ forms/rhf-with-zod/src/pages/Cadastro/CadastroPessoal.tsx
@@ -26,24 +26,29 @@ function captalizeText(text: string) {
   return captalizedWords.join(" ");
 }
 
-const cadastroSchema = z.object({
-  nome: z
-    .string()
-    .min(1, "Este campo é obrigatório")
-    .min(5, "O nome deve ter pelo menos cinco caracteres")
-    .transform(captalizeText),
-  email: z
-    .string()
-    .min(1, "Este campo é obrigatório")
-    .email("O e-mail não é válido")
-    .toLowerCase(),
-  telefone: z.string().min(1, "Este campo é obrigatório"),
-  senha: z
-    .string()
-    .min(1, "Este campo é obrigatório")
-    .min(8, "A senha deve ter pelo menos oito caracteres"),
-  senhaVerificada: z.string().min(1, "Este campo é obrigatório"),
-});
+const cadastroSchema = z
+  .object({
+    nome: z
+      .string()
+      .min(1, "Este campo é obrigatório")
+      .min(5, "O nome deve ter pelo menos cinco caracteres")
+      .transform(captalizeText),
+    email: z
+      .string()
+      .min(1, "Este campo é obrigatório")
+      .email("O e-mail não é válido")
+      .toLowerCase(),
+    telefone: z.string().min(1, "Este campo é obrigatório"),
+    senha: z
+      .string()
+      .min(1, "Este campo é obrigatório")
+      .min(8, "A senha deve ter pelo menos oito caracteres"),
+    senhaVerificada: z.string().min(1, "Este campo é obrigatório"),
+  })
+  .refine(({ senha, senhaVerificada }) => senha === senhaVerificada, {
+    message: "As senhas não correspondem",
+    path: ["senhaVerificada"],
+  });
 
 type FormInputTipos = z.infer<typeof cadastroSchema>;
 
