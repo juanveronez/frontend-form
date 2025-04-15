f47f5989472e445c9734931525461a46c861dde6
Juan Veronez <juan.monteirov@gmail.com>
2025-04-15 08:08:42 -0300

    feat(rhf-with-zod): register fields, add error messages and error state to inputs, add zodResolver too

--- forms/rhf-with-zod/src/pages/Cadastro/CadastroEspecialistaEndereco.tsx
+++ forms/rhf-with-zod/src/pages/Cadastro/CadastroEspecialistaEndereco.tsx
@@ -2,6 +2,7 @@ import { z } from "zod";
 import {
   Button,
   Divisor,
+  ErrorMessage,
   Fieldset,
   Form,
   FormContainer,
@@ -15,13 +16,14 @@ import {
   UploadTitulo,
 } from "../../components";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const especialistaEnderecoSchema = z.object({
   endereco: z.object({
     cep: z
       .string()
       .min(1, "Este campo é obrigatório")
-      .min(9, "Informe um CEP válido"),
+      .length(9, "Informe um CEP válido"),
     rua: z.string().min(1, "Este campo é obrigatório"),
     numero: z.coerce
       .number({
@@ -36,7 +38,12 @@ const especialistaEnderecoSchema = z.object({
 type FormEspecialistaEndereco = z.infer<typeof especialistaEnderecoSchema>;
 
 const CadastroEspecialistaEndereco = () => {
-  useForm<FormEspecialistaEndereco>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormEspecialistaEndereco>({
+    resolver: zodResolver(especialistaEnderecoSchema),
     defaultValues: {
       endereco: {
         bairro: "",
@@ -48,10 +55,14 @@ const CadastroEspecialistaEndereco = () => {
     },
   });
 
+  const handleValid = (data: FormEspecialistaEndereco) => {
+    console.log(data);
+  };
+
   return (
     <>
       <Titulo className="titulo">Para finalizar, só alguns detalhes!</Titulo>
-      <Form>
+      <Form onSubmit={handleSubmit(handleValid)}>
         <>
           <UploadTitulo>Sua foto</UploadTitulo>
           <UploadLabel htmlFor="campo-upload">
@@ -64,21 +75,57 @@ const CadastroEspecialistaEndereco = () => {
         <Divisor />
         <Fieldset>
           <Label htmlFor="campo-cep">CEP</Label>
-          <Input id="campo-cep" placeholder="Insira seu CEP" type="text" />
+          <Input
+            id="campo-cep"
+            placeholder="Insira seu CEP"
+            type="text"
+            $error={!!errors.endereco?.cep}
+            {...register("endereco.cep")}
+          />
+          {errors.endereco?.cep && (
+            <ErrorMessage>{errors.endereco.cep.message}</ErrorMessage>
+          )}
         </Fieldset>
         <Fieldset>
           <Label htmlFor="campo-rua">Rua</Label>
-          <Input id="campo-rua" placeholder="Rua Agarikov" type="text" />
+          <Input
+            id="campo-rua"
+            placeholder="Rua Agarikov"
+            type="text"
+            $error={!!errors.endereco?.rua}
+            {...register("endereco.rua")}
+          />
+          {errors.endereco?.rua && (
+            <ErrorMessage>{errors.endereco.rua.message}</ErrorMessage>
+          )}
         </Fieldset>
 
         <FormContainer>
           <Fieldset>
             <Label htmlFor="campo-numero-rua">Número</Label>
-            <Input id="campo-numero-rua" placeholder="Ex: 1440" type="text" />
+            <Input
+              id="campo-numero-rua"
+              placeholder="Ex: 1440"
+              type="text"
+              $error={!!errors.endereco?.numero}
+              {...register("endereco.numero")}
+            />
+            {errors.endereco?.numero && (
+              <ErrorMessage>{errors.endereco.numero.message}</ErrorMessage>
+            )}
           </Fieldset>
           <Fieldset>
             <Label htmlFor="campo-bairro">Bairro</Label>
-            <Input id="campo-bairro" placeholder="Vila Mariana" type="text" />
+            <Input
+              id="campo-bairro"
+              placeholder="Vila Mariana"
+              type="text"
+              $error={!!errors.endereco?.bairro}
+              {...register("endereco.bairro")}
+            />
+            {errors.endereco?.bairro && (
+              <ErrorMessage>{errors.endereco.bairro.message}</ErrorMessage>
+            )}
           </Fieldset>
         </FormContainer>
         <Fieldset>
@@ -87,7 +134,12 @@ const CadastroEspecialistaEndereco = () => {
             id="campo-localidade"
             placeholder="São Paulo, SP"
             type="text"
+            $error={!!errors.endereco?.localidade}
+            {...register("endereco.localidade")}
           />
+          {errors.endereco?.localidade && (
+            <ErrorMessage>{errors.endereco.localidade.message}</ErrorMessage>
+          )}
         </Fieldset>
         <Button type="submit">Cadastrar</Button>
       </Form>
