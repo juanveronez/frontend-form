c2939e1bb7004a641e1d3b21ed528a168626f951
Juan Veronez <juan.monteirov@gmail.com>
2025-04-20 10:22:40 -0300

    feat(solid-react): update Button and Input components using (LSP)
    - Liskov Substitution Principle (LSP) is a object-oriented principle
      that should be used in React components to ensure that a component
      can replace its parent component without affecting the behavior.
    - Update Button to use all html button attr, ensuring that the
      component can be used as a functional button component.
    - Ensure Input component adheres to LSP by allowing it to accept
      all standard input attributes and behave as a standard input element.
    - Following this principle both components are now more flexible
      and can be used in a wider range of scenarios.

--- fe-architecture/solid-react/src/components/Button/index.tsx
+++ fe-architecture/solid-react/src/components/Button/index.tsx
@@ -1,32 +1,18 @@
-import { CSSProperties, MouseEvent, ReactNode } from "react";
+import { ButtonHTMLAttributes } from "react";
 import classnames from "classnames"; // Para combinar classes dinamicamente
 import Styles from "./Button.module.css";
 
-type ButtonProps = {
-  style?: CSSProperties;
-  children?: ReactNode;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   variant?: "primary" | "secondary";
   size?: "small" | "medium" | "large"; // Define diferentes tamanhos
-  onClick: (e: MouseEvent<HTMLElement>) => void; // Manipulação de click adicional
 };
 
 const Button = ({
-  children,
   variant = "primary",
   size = "medium",
-
-  onClick,
-  style,
   ...props
 }: ButtonProps) => (
-  <button
-    style={style}
-    className={classnames(Styles[variant], Styles[size])}
-    onClick={onClick}
-    {...props}
-  >
-    {children}
-  </button>
+  <button className={classnames(Styles[variant], Styles[size])} {...props} />
 );
 
 export default Button;
--- fe-architecture/solid-react/src/components/Input/index.tsx
+++ fe-architecture/solid-react/src/components/Input/index.tsx
@@ -1,34 +1,14 @@
-import { CSSProperties } from "react";
+import { InputHTMLAttributes } from "react";
 import Styles from "./Input.module.css";
 
-export type InputProps = {
+export type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   variant?: "primary" | "secondary";
-  placeholder?: string;
-  value?: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  id?: string;
-  style?: CSSProperties;
-  type?: string;
 };
 
 const Input = ({
   variant = "primary",
-  onChange,
-  placeholder,
-  id,
-  style,
   type = "text",
   ...props
-}: InputProps) => (
-  <input
-    type={type}
-    style={style}
-    className={Styles[variant]}
-    {...props}
-    onChange={() => onChange}
-    id={id}
-    placeholder={placeholder}
-  />
-);
+}: InputProps) => <input type={type} className={Styles[variant]} {...props} />;
 
 export default Input;
