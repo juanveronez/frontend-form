452494a7fb2aa734ad50b525a832c9c9d9ee65bf
Juan Veronez <juan.monteirov@gmail.com>
2025-04-22 07:06:28 -0300

    build(clean-arch-react): add react-toastify and make the setup for it

--- fe-architecture/clean-arch-react/src/RootLayout.tsx
+++ fe-architecture/clean-arch-react/src/RootLayout.tsx
@@ -1,25 +1,26 @@
-import styled from "styled-components"
-import { Header } from "./components/Header"
-import { Outlet } from "react-router"
+import styled from "styled-components";
+import { Header } from "./components/Header";
+import { Outlet } from "react-router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.css";
 
 const Container = styled.div`
   display: flex;
   gap: 24px;
   width: 1200px;
   margin: 24px auto;
-`
-
+`;
 
 const RootLayout = () => {
+  return (
+    <>
+      <ToastContainer />
+      <Header />
+      <Container>
+        <Outlet />
+      </Container>
+    </>
+  );
+};
 
-    return (
-        <>
-            <Header />
-            <Container>
-                <Outlet />
-            </Container>
-        </>
-    )
-}
-
-export default RootLayout
+export default RootLayout;
